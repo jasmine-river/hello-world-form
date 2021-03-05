7832e21eeef5ed51197d5a10eaa4bba55ed37c62
Jasmine Jiang <headlessnun@gmail.com>
2021-03-05 16:19:56 +0000

    Post form data to the Quote and Pay tool and retrieve invoice URL

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,22 +2,25 @@ import React from "react"
 import axios from "axios"
 
 class IndexPage extends React.Component {
-  state = {
-    title_of_quote: "QUOTE & PAY",
-    fullname: "",
-    companyname: "",
-    companyaddress1: "",
-    companyaddress2: "",
-    companyaddress3: "",
-    companyaddress4: "",
-    servicedescription: "",
-    sub_end_date: "",
-    contractterm: "",
-    amount: 0,
-    servicenote: "",
-    oneTimeAmount: 0,
-    onetimenote: "",
-    vat: 23,
+  constructor(props) {
+    super(props)
+    this.state = {
+      title_of_quote: "QUOTE & PAY",
+      fullname: "",
+      companyname: "",
+      companyaddress1: "",
+      companyaddress2: "",
+      companyaddress3: "",
+      companyaddress4: "",
+      servicedescription: "",
+      sub_end_date: "",
+      contractterm: "",
+      amount: 0,
+      servicenote: "",
+      OneTimeAmount: 0,
+      onetimenote: "",
+      vat: 23,
+    }
   }
 
   isChecked = event => {
@@ -36,139 +39,6 @@ class IndexPage extends React.Component {
     }
   }
 
-  // isValidDate = (event, date) => {
-  //   var dateString = date
-  //   // First check for the pattern
-  //   if (!/^\d{4}\/\d{1,2}\/\d{1,2}$/.test(dateString)) return false
-  //
-  //   console.log("date format validation passed")
-  //
-  //   // Parse the date parts to integers
-  //   var parts = dateString.split("/")
-  //   var day = parseInt(parts[2], 10)
-  //   var month = parseInt(parts[1], 10)
-  //   var year = parseInt(parts[0], 10)
-  //
-  //   var currentDate = new Date()
-  //   var currentDay = String(currentDate.getDate()).padStart(2, "0")
-  //   var currentMonth = String(currentDate.getMonth() + 1).padStart(2, "0")
-  //   var currentYear = currentDate.getFullYear()
-  //
-  //   // Check the ranges of month and year
-  //   if (
-  //     year < currentYear ||
-  //     (year < currentYear && month < currentMonth && day < currentDay) ||
-  //     month == 0 ||
-  //     month > 12
-  //   )
-  //     return false
-  //
-  //   console.log("date range validation passed")
-  //
-  //   var monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  //
-  //   // Adjust for leap years
-  //   if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
-  //     monthLength[1] = 29
-  //
-  //   // Check the range of the day
-  //   return day > 0 && day <= monthLength[month - 1]
-  // }
-  //
-  // validateInvoice = event => {
-  //   let sub_end_date = this.state.sub_end_date
-  //
-  //   console.log(sub_end_date)
-  //   console.log("validating values...")
-  //
-  //   if (this.isValidDate(sub_end_date)) {
-  //     console.log("valid!")
-  //     this.handleSubmit()
-  //   } else {
-  //     console.log("not valid!")
-  //     alert(
-  //       "Invalid date entered. Make sure that it is past the current date and in the format YYYY/MM/DD."
-  //     )
-  //   }
-  // }
-
-  // copyUrl = (event, invoiceURL) => {
-  //   let url = document.createElement("input"),
-  //     text = "quotedandpay.xiir.com/" + invoiceURL
-  //
-  //   document.body.appendChild(url)
-  //   url.value = text
-  //
-  //   url.select()
-  //   document.execCommand("copy")
-  //   document.body.removeChild(url)
-  //
-  //   alert("Copied the url: " + text)
-  // }
-  //
-  // createPopUpBox = (event, invoiceURL) => {
-  //   console.log(invoiceURL)
-  //
-  //   let form = document.getElementsByClassName("subscriptionForm")[0]
-  //
-  //   //pop up box
-  //   let popUpBox = document.createElement("div")
-  //   popUpBox.setAttribute("id", "invoiceModal")
-  //   popUpBox.setAttribute("class", "modal")
-  //
-  //   //div holding content inside pop up box
-  //   let modalContent = document.createElement("div")
-  //   modalContent.setAttribute("class", "modalContent")
-  //   popUpBox.appendChild(modalContent)
-  //
-  //   //close button
-  //   let span = document.createElement("span")
-  //   span.setAttribute("class", "close")
-  //   let spanText = document.createTextNode("X")
-  //   span.appendChild(spanText)
-  //   modalContent.appendChild(span)
-  //
-  //   //verification text
-  //   let center = document.createElement("center")
-  //   let textBox = document.createElement("p")
-  //   let text = document.createTextNode("Form has been successfully submitted")
-  //   textBox.appendChild(text)
-  //   center.appendChild(textBox)
-  //   modalContent.appendChild(center)
-  //
-  //   //copy button
-  //   let copyButton = document.createElement("button")
-  //   copyButton.setAttribute("class", "btn btn-primary login-btn btn-block")
-  //   copyButton.addEventListener("click", function () {
-  //     this.copyUrl(invoiceURL)
-  //   })
-  //   let copyButtonText = document.createTextNode("Copy Invoice URL")
-  //   copyButton.appendChild(copyButtonText)
-  //   modalContent.appendChild(copyButton)
-  //
-  //   //redirect button
-  //   let redirectButton = document.createElement("button")
-  //   redirectButton.setAttribute("class", "btn btn-primary login-btn btn-block")
-  //   redirectButton.setAttribute("style", "margin-top: 10px;")
-  //   redirectButton.addEventListener("click", function () {
-  //     window.location = "quotedandpay.xiir.com/" + invoiceURL
-  //   })
-  //   let redirectButtonText = document.createTextNode("Go To Invoice")
-  //   redirectButton.appendChild(redirectButtonText)
-  //   modalContent.appendChild(redirectButton)
-  //
-  //   //add popup box to form
-  //   form.appendChild(popUpBox)
-  //
-  //   //display modal
-  //   popUpBox.style.display = "block"
-  //
-  //   //close modal when X is clicked
-  //   span.onclick = function () {
-  //     popUpBox.style.display = "none"
-  //   }
-  // }
-
   handleInputChange = event => {
     const target = event.target
     const value = target.value
@@ -181,17 +51,34 @@ class IndexPage extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault()
-    alert(`title: ${this.state.title_of_quote} name: ${this.state.fullname} company: ${this.state.companyname}
-      address: ${this.state.companyaddress1} ${this.state.companyaddress2} ${this.state.companyaddress3} ${this.state.companyaddress4}
-      description: ${this.state.servicedescription} sub_end_date: ${this.state.sub_end_date} contractterm: ${this.state.contractterm}
-      sub amount: ${this.state.amount} serv note: ${this.state.servicenote}
-      onetime amount: ${this.state.oneTimeAmount} onetime note: ${this.state.onetimenote} vat: ${this.state.vat}
+    alert(`title_of_quote: ${this.state.title_of_quote} fullname: ${this.state.fullname} companyname: ${this.state.companyname}
+      companyaddress: ${this.state.companyaddress1} ${this.state.companyaddress2} ${this.state.companyaddress3} ${this.state.companyaddress4}
+      servicedescription: ${this.state.servicedescription} sub_end_date: ${this.state.sub_end_date} contractterm: ${this.state.contractterm}
+      amount: ${this.state.amount} servicenote: ${this.state.servicenote}
+      OneTimeAmount: ${this.state.OneTimeAmount} onetimenote: ${this.state.onetimenote} vat: ${this.state.vat}
       `)
+
+    let bodyFormData = new FormData()
+    bodyFormData.append("title_of_quote", this.state.title_of_quote)
+    bodyFormData.append("fullname", this.state.fullname)
+    bodyFormData.append("companyname", this.state.companyname)
+    bodyFormData.append("companyaddress1", this.state.companyaddress1)
+    bodyFormData.append("companyaddress2", this.state.companyaddress2)
+    bodyFormData.append("companyaddress3", this.state.companyaddress3)
+    bodyFormData.append("companyaddress4", this.state.companyaddress4)
+    bodyFormData.append("servicedescription", this.state.servicedescription)
+    bodyFormData.append("sub_end_date", this.state.sub_end_date)
+    bodyFormData.append("contractterm", this.state.contractterm)
+    bodyFormData.append("amount", this.state.amount)
+    bodyFormData.append("servicenote", this.state.servicenote)
+    bodyFormData.append("OneTimeAmount", this.state.OneTimeAmount)
+    bodyFormData.append("onetimenote", this.state.onetimenote)
+    bodyFormData.append("vat", this.state.vat)
 
     axios({
       method: "post",
       url: "https://quotedandpay.xiir.com/submit-invoice.php",
-      data: this.state,
+      data: bodyFormData,
     })
       .then(function (response) {
         console.log("Submitted")
@@ -207,7 +94,7 @@ class IndexPage extends React.Component {
 
   render() {
     return (
-      <form className="subscriptionForm">
+      <form onSubmit={this.handleSubmit}>
         <input
           id="title_of_quote"
           type="text"
@@ -221,7 +108,6 @@ class IndexPage extends React.Component {
           type="text"
           name="fullname"
           placeholder="Full Name"
-          value={this.state.fullname}
           onChange={this.handleInputChange}
         />
         <input
@@ -229,7 +115,6 @@ class IndexPage extends React.Component {
           type="text"
           name="companyname"
           placeholder="Company Name"
-          value={this.state.companyname}
           onChange={this.handleInputChange}
         />
         <input
@@ -237,7 +122,6 @@ class IndexPage extends React.Component {
           type="text"
           name="companyaddress1"
           placeholder="Company Address Line 1"
-          value={this.state.companyaddress1}
           onChange={this.handleInputChange}
         />
         <input
@@ -245,7 +129,6 @@ class IndexPage extends React.Component {
           type="text"
           name="companyaddress2"
           placeholder="Company Address Line 2"
-          value={this.state.companyaddress2}
           onChange={this.handleInputChange}
         />
         <input
@@ -253,7 +136,6 @@ class IndexPage extends React.Component {
           type="text"
           name="companyaddress3"
           placeholder="Company Address Line 3"
-          value={this.state.companyaddress3}
           onChange={this.handleInputChange}
         />
         <input
@@ -261,7 +143,6 @@ class IndexPage extends React.Component {
           type="text"
           name="companyaddress4"
           placeholder="Company Address Line 4"
-          value={this.state.companyaddress4}
           onChange={this.handleInputChange}
         />
         <input
@@ -269,17 +150,18 @@ class IndexPage extends React.Component {
           type="text"
           name="servicedescription"
           placeholder="Service Description"
-          value={this.state.servicedescription}
           onChange={this.handleInputChange}
         />
+        <label for="sub_end_date">Subscription End Date:</label>
         <input
           id="sub_end_date"
-          type="text"
+          type="date"
           name="sub_end_date"
-          placeholder="Subscription End Date (YYYY/MM/DD)"
+          min={new Date().toISOString().split("T")[0]} // Set the minimum date to today
+          onKeyDown={e => e.preventDefault()} // Disable keyboard input for date
           required
-          value={this.state.sub_end_date}
           onChange={this.handleInputChange}
+          //placeholder="Subscription End Date (YYYY/MM/DD)"
         />
 
         <hr />
@@ -291,7 +173,7 @@ class IndexPage extends React.Component {
           value="daily"
           onChange={this.handleInputChange}
         />
-        <label htmlFor="contractterm"> Daily </label>
+        <label htmlFor="contracttermDaily"> Daily </label>
         <input
           type="radio"
           onClick={this.isChecked}
@@ -300,7 +182,7 @@ class IndexPage extends React.Component {
           value="weekly"
           onChange={this.handleInputChange}
         />
-        <label htmlFor="contractterm"> Weekly </label>
+        <label htmlFor="contracttermWeekly"> Weekly </label>
         <input
           type="radio"
           onClick={this.isChecked}
@@ -309,7 +191,7 @@ class IndexPage extends React.Component {
           value="monthly"
           onChange={this.handleInputChange}
         />
-        <label htmlFor="contractterm">Monthly</label>
+        <label htmlFor="contracttermMonthly">Monthly</label>
         <input
           type="radio"
           onClick={this.isChecked}
@@ -318,7 +200,7 @@ class IndexPage extends React.Component {
           value="3-months"
           onChange={this.handleInputChange}
         />
-        <label htmlFor="contractterm"> Every 3 months </label>
+        <label htmlFor="contractterm3Months"> Every 3 months </label>
         <input
           type="radio"
           onClick={this.isChecked}
@@ -327,7 +209,7 @@ class IndexPage extends React.Component {
           value="bi-annually"
           onChange={this.handleInputChange}
         />
-        <label htmlFor="contractterm"> Bi-Annually </label>
+        <label htmlFor="contracttermBiAnnually"> Bi-Annually </label>
         <input
           type="radio"
           onClick={this.isChecked}
@@ -336,7 +218,7 @@ class IndexPage extends React.Component {
           value="annually"
           onChange={this.handleInputChange}
         />
-        <label htmlFor="contractterm"> Annually </label>
+        <label htmlFor="contracttermAnnually"> Annually </label>
 
         <hr />
         <label htmlFor="amount">Subscription Fee:</label>
@@ -355,11 +237,11 @@ class IndexPage extends React.Component {
           rows="3"
           onChange={this.handleInputChange}
         ></textarea>
-        <label htmlFor="amount">Once-off Payment:</label>
+        <label htmlFor="OneTimeAmount">Once-off Payment:</label>
         <input
           type="number"
-          name="oneTimeAmount"
-          id="oneTimeAmount"
+          name="OneTimeAmount"
+          id="OneTimeAmount"
           placeholder="5.00"
           min="0"
           onChange={this.handleInputChange}
@@ -371,9 +253,9 @@ class IndexPage extends React.Component {
           rows="3"
           onChange={this.handleInputChange}
         ></textarea>
-        <label htmlFor="amount">VAT(in %):</label>
+        <label htmlFor="vat">VAT(in %):</label>
         <input
-          type="text"
+          type="number"
           name="vat"
           id="vat"
           value={this.state.vat}
@@ -382,9 +264,7 @@ class IndexPage extends React.Component {
           onChange={this.handleInputChange}
         />
 
-        <button type="button" onClick={this.handleSubmit}>
-          Submit
-        </button>
+        <button type="submit">Submit</button>
       </form>
     )
   }
